6983436ac8358358715aea8d0af840405b6ee5e1
BimaTristan <2141720077@student.polinema.ac.id>
2022-03-02 22:41:51 +0700

    Tugas3

---
+++ praktikum3/jbTanah.java
@@ -0,0 +1,11 @@
+public class jbTanah {
+    public int panjang, lebar;
+
+    public int luasTanah() {
+        return panjang * lebar;
+    }
+
+    public int tanahTerluas(int[] luas) {
+        return 0;
+    }
+}
---
+++ praktikum3/kubus.java
@@ -0,0 +1,15 @@
+public class kubus {
+    public int sisi;
+
+    public kubus(int s) {
+        sisi = s;
+    }
+
+    public int volume() {
+        return sisi * sisi * sisi;
+    }
+
+    public int luasPermukaan() {
+        return 6 * (sisi * sisi * sisi);
+    }
+}
