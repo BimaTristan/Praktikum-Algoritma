27fbc9d5e5f74eaf6b76ec70a73aa81b3b1caae9
BimaTristan <99968352+BimaTristan@users.noreply.github.com>
2022-04-28 09:20:11 +0700

    Add files via upload

---
+++ Pertemuan10/Tugas/Tugas2Mahasiswa.java
@@ -0,0 +1,19 @@
+package Tugas;
+
+public class Tugas2Mahasiswa {
+    String nim;
+    String nama;
+    int absen;
+    double ipk;
+
+    Tugas2Mahasiswa(String nim, String nama, int absen, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.absen = absen;
+        this.ipk = ipk;
+    }
+
+    Tugas2Mahasiswa() {
+
+    }
+}
