c40a711db7a8a684307dab5f912f11ecdef9b598
BimaTristan <99968352+BimaTristan@users.noreply.github.com>
2022-05-26 10:21:26 +0700

    Add files via upload

---
+++ Pertemuan12/Praktikum/doublelinkedlists/DoubleLinkedListMain.java
@@ -0,0 +1,10 @@
+package Praktikum.doublelinkedlists;
+
+public class DoubleLinkedListMain {
+    public static void main(String[] args) {
+
+    }
+    DoubleLinkedList dll = new DoubleLinkedList();
+    dll.print();
+    System.out.println("Size : " +dll.size());
+}
